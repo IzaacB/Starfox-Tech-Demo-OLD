48902ffd460288d28ed252575d2e483d93d79d82
IzaacB <47872628+IzaacB@users.noreply.github.com>
2023-08-08 15:09:35 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,44 +4,17 @@ import time
 import mesh_lib
 pygame.init()
 
-canvas = pygame.display.set_mode((256, 224), pygame.SCALED, vsync=1)
+canvas = pygame.display.set_mode((256, 224), pygame.SCALED, vsync=0)
 refresh = pygame.time.Clock()
-refresh_rate = 60
+refresh_rate = 20
+background = pygame.image.load("Corneria.png")
+background_x, background_y = 0, -248
 
 viewport_pos = [0, 0, 0]
 viewport_rot = [0, 0, 0]
 viewport_width = 1
 viewport_height = canvas.get_height()/canvas.get_width()
-viewport_distance = .5
-
-def draw_triangle(p0, p1, p2, color):
-    x0, y0 = p0[0], p0[1]
-    x1, y1 = p1[0], p1[1]
-    x2, y2 = p2[0], p2[1] 
-
-    pygame.draw.polygon(canvas, color, ((canvas.get_width()/2 + x0, canvas.get_height()/2 - y0),
-                                        (canvas.get_width()/2 + x1, canvas.get_height()/2 - y1),
-                                        (canvas.get_width()/2 + x2, canvas.get_height()/2 - y2)))
-
-def sort_triangles(triangle_list):
-    for i in range(0, len(triangle_list)):
-        for j in range(i, len(triangle_list)):
-            origin2 = [(triangle_list[j][0][0] + triangle_list[j][1][0] + triangle_list[j][2][0])/3,
-                       (triangle_list[j][0][1] + triangle_list[j][1][1] + triangle_list[j][2][1])/3,
-                       (triangle_list[j][0][2] + triangle_list[j][1][2] + triangle_list[j][2][2])/3]
-            
-            origin1 = [(triangle_list[i][0][0] + triangle_list[i][1][0] + triangle_list[i][2][0])/3,
-                       (triangle_list[i][0][1] + triangle_list[i][1][1] + triangle_list[i][2][1])/3,
-                       (triangle_list[i][0][2] + triangle_list[i][1][2] + triangle_list[i][2][2])/3]
-            
-            distance2 = math.sqrt(origin2[2] **2 + origin2[1] ** 2 + origin2[0] ** 2)
-
-            distance1 = math.sqrt(origin1[2] **2 + origin1[1] ** 2 + origin1[0] ** 2)
-
-            if distance1 < distance2:
-                cache = triangle_list[i]
-                triangle_list[i] = triangle_list[j]
-                triangle_list[j] = cache
+viewport_distance = 1
 
 def scale_to_canvas(x, y):
     #Scale points from 3D coordintates to fit on canvas.
@@ -53,9 +26,44 @@ def project_vertex(p): #vertex coordinate format: [x, y, z].
     x1, y1 = (x * viewport_distance)/z, (y * viewport_distance)/z#Projection formula.
     return scale_to_canvas(x1, y1)
 
+def draw_triangle(p0, p1, p2, color):
+    x0, y0 = p0[0], p0[1]
+    x1, y1 = p1[0], p1[1]
+    x2, y2 = p2[0], p2[1] 
+
+    pygame.draw.polygon(canvas, color, ((canvas.get_width()/2 + x0, canvas.get_height()/2 - y0),
+                                        (canvas.get_width()/2 + x1, canvas.get_height()/2 - y1),
+                                        (canvas.get_width()/2 + x2, canvas.get_height()/2 - y2)))
+
+def sort_triangles(triangle_list):
+    pass_limit = 100
+    passes = 0
+    while passes <= pass_limit:
+        for i in range(0, len(triangle_list) - 1):
+            j = i + 1
+            
+            origin1 = [(triangle_list[j][0][0] + triangle_list[j][1][0] + triangle_list[j][2][0])/3,
+                        (triangle_list[j][0][1] + triangle_list[j][1][1] + triangle_list[j][2][1])/3,
+                        (triangle_list[j][0][2] + triangle_list[j][1][2] + triangle_list[j][2][2])/3]
+            
+            origin2 = [(triangle_list[i][0][0] + triangle_list[i][1][0] + triangle_list[i][2][0])/3,
+                        (triangle_list[i][0][1] + triangle_list[i][1][1] + triangle_list[i][2][1])/3,
+                        (triangle_list[i][0][2] + triangle_list[i][1][2] + triangle_list[i][2][2])/3]
+            
+            distance1 = math.sqrt(origin2[2] **2 + origin2[1] ** 2 + origin2[0] ** 2)
+
+            distance2 = math.sqrt(origin1[2] **2 + origin1[1] ** 2 + origin1[0] ** 2)
+            
+
+            if distance1 < distance2:
+                cache = triangle_list[i]
+                triangle_list[i] = triangle_list[j]
+                triangle_list[j] = cache
+
+        passes += 1
 #Initialize game objects:
-cube = mesh_lib.cube(0, 0, 10)
-cube2 = mesh_lib.cube(0, 0, 5)
+arwing = mesh_lib.arwing(0, 0, 10)
+
 #Start the main loop:
 main_loop = True
 while main_loop:
@@ -65,41 +73,45 @@ while main_loop:
 
     key_state = pygame.key.get_pressed()
     delta_time = refresh.tick(refresh_rate)/1000
+    if key_state[pygame.K_q]:
+        main_loop = False
 
     if key_state[pygame.K_w]:
-        cube.transform([0, 0, -10], delta_time)
-        cube2.transform([0, 0, -10], delta_time)
-    
+        arwing.transform([0, 0, -5], delta_time)
+
     if key_state[pygame.K_s]:
-        cube.transform([0, 0, 10], delta_time)
-        cube2.transform([0, 0, 10], delta_time)
+        arwing.transform([0, 0, 5], delta_time)
 
     if key_state[pygame.K_a]:
-        cube.transform([10, 0, 0], delta_time)
-        cube2.transform([10, 0, 0], delta_time)
-    
+        arwing.transform([5, 0, 0], delta_time)
+
     if key_state[pygame.K_d]:
-        cube.transform([-10, 0, 0], delta_time)
-        cube2.transform([-10, 0, 0], delta_time)
+        arwing.transform([-5, 0, 0], delta_time)
+
+    if key_state[pygame.K_UP]:
+        arwing.transform([0, -5, 0], delta_time)
+
+    if key_state[pygame.K_DOWN]:
+        arwing.transform([0, 5, 0], delta_time)
 
     if key_state[pygame.K_RIGHT]:
-        cube.rotate_global([0, -2, 0], delta_time)
-        cube2.rotate_global([0, -2, 0], delta_time)
-    
+        arwing.rotate_global([0, -1, 0], delta_time)
+
     if key_state[pygame.K_LEFT]:
-        cube.rotate_global([0, 2, 0], delta_time)
-        cube2.rotate_global([0, 2, 0], delta_time)
+        arwing.rotate_global([0, 1, 0], delta_time)
 
     #Clear render layers:
     render_layer1 = []
-    
-    #Add game objects to render:
-    render_layer1 += cube.render()
-    render_layer1 += cube2.render()
+    render_layer1 += arwing.render()
 
     #Perform layer functions:
-    canvas.fill((0, 0, 0))
     sort_triangles(render_layer1)
+
+    #Start render loop:
+    canvas.fill((0, 0, 0))
+    canvas.blit(background, (background_x, background_y))
+
     for i in range(0,len(render_layer1)):
         draw_triangle(project_vertex(render_layer1[i][0]), project_vertex(render_layer1[i][1]), project_vertex(render_layer1[i][2]), render_layer1[i][3])
+
     pygame.display.flip()
--- mesh_lib.py
+++ mesh_lib.py
@@ -43,19 +43,75 @@ class cube():
             self.vertices[i][0] = self.new_x
             self.vertices[i][1] = self.new_y
             self.vertices[i][2] = self.new_z
+
+    def render(self):
+        self.aperature = 1
+        self.temp_vertices = copy.deepcopy(self.vertices)
+
+        for i in range(0, len(self.temp_vertices)):
+            if self.temp_vertices[i][2] <= self.aperature:
+                self.temp_vertices[i][2] = self.aperature
+
+        self.tris =    [[self.temp_vertices[0], self.temp_vertices[1], self.temp_vertices[2], (255, 0, 0)],
+                        [self.temp_vertices[0], self.temp_vertices[3], self.temp_vertices[2], (255, 0, 0)],
+                        [self.temp_vertices[4], self.temp_vertices[0], self.temp_vertices[3], (225, 0, 0)],
+                        [self.temp_vertices[4], self.temp_vertices[7], self.temp_vertices[3], (225, 0, 0)],
+                        [self.temp_vertices[1], self.temp_vertices[5], self.temp_vertices[6], (200, 0, 0)],
+                        [self.temp_vertices[1], self.temp_vertices[2], self.temp_vertices[6], (200, 0, 0)],
+                        [self.temp_vertices[5], self.temp_vertices[4], self.temp_vertices[7], (150, 0, 0)],
+                        [self.temp_vertices[5], self.temp_vertices[6], self.temp_vertices[7], (150, 0, 0)],
+                        [self.temp_vertices[0], self.temp_vertices[4], self.temp_vertices[5], (125, 0, 0)],
+                        [self.temp_vertices[0], self.temp_vertices[1], self.temp_vertices[5], (125, 0, 0)],
+                        [self.temp_vertices[3], self.temp_vertices[2], self.temp_vertices[6], (100, 0, 0)],
+                        [self.temp_vertices[3], self.temp_vertices[7], self.temp_vertices[6], (100, 0, 0)],]
+        
+        self.filter_tris = []
+        
+        for i in range(0, len(self.tris)):
+            if self.tris[i][0][2] > self.aperature or self.tris[i][1][2] > self.aperature or self.tris[i][2][2] > self.aperature:
+                self.filter_tris.append(self.tris[i])
+
+        return self.filter_tris
     
-    def rotate_local(self, rotation, delta_time):
+class arwing():
+    def __init__(self, x, y, z):
+        self.vertices = [[0 + x, 0 + y, 3 + z],
+                         [x -.5, 0 + y, 0 + z],
+                         [0 + x, .5 + y, 0 + z],
+                         [.5 + x, 0 + y, 0 + z],
+                         [0 + x, .25 + y, -.5 + z],
+                         [0 + x, .15 + y, -.25 + z],
+                         [x - 1, y - .25, z + 1.5],
+                         [x - 1.25, y + .75, z - .75], 
+                         [x - 1, y, z], 
+                         [x - .8, y - .15, z + .25], 
+                         [x - .8, y - .15, z - .25],
+                         [x - 2, y - .5, z],
+                         [x + 1, y - .25, z + 1.5],
+                         [x + 1.25, y + .75, z - .75], 
+                         [x + 1, y, z], 
+                         [x + .8, y - .15, z + .25], 
+                         [x + .8, y - .15, z - .25],
+                         [x + 2, y - .5, z]]
+        
+    def transform(self, transformation, delta_time):
+        for i in range(0, len(self.vertices)):
+            self.vertices[i][0] += transformation[0] * delta_time
+            self.vertices[i][1] += transformation[1] * delta_time
+            self.vertices[i][2] += transformation[2] * delta_time
+
+    def rotate_global(self, rotation, delta_time):
         #X rotation
         for i in range(0, len(self.vertices)):
             self.new_x = self.vertices[i][0]
-            self.new_y = math.cos(rotation[0] * delta_time) * self.vertices[i][1] - math.sin(rotation[0] * delta_time) * self.vertices[i][2] + rotation[0] * 10 * delta_time
+            self.new_y = math.cos(rotation[0] * delta_time) * self.vertices[i][1] - math.sin(rotation[0] * delta_time) * self.vertices[i][2]
             self.new_z = math.sin(rotation[0] * delta_time) * self.vertices[i][1] + math.cos(rotation[0] * delta_time) * self.vertices[i][2]
             self.vertices[i][0] = self.new_x
             self.vertices[i][1] = self.new_y
             self.vertices[i][2] = self.new_z
         #Y rotation
         for i in range(0, len(self.vertices)):
-            self.new_x = math.cos(rotation[1] * delta_time) * self.vertices[i][0] + math.sin(rotation[1] * delta_time) * self.vertices[i][2] - rotation[1] * 10 * delta_time
+            self.new_x = math.cos(rotation[1] * delta_time) * self.vertices[i][0] + math.sin(rotation[1] * delta_time) * self.vertices[i][2]
             self.new_y = self.vertices[i][1]
             self.new_z = -math.sin(rotation[1] * delta_time) * self.vertices[i][0] + math.cos(rotation[1] * delta_time) * self.vertices[i][2]
             self.vertices[i][0] = self.new_x
@@ -78,18 +134,28 @@ class cube():
             if self.temp_vertices[i][2] <= self.aperature:
                 self.temp_vertices[i][2] = self.aperature
 
-        self.tris =    [[self.temp_vertices[0], self.temp_vertices[1], self.temp_vertices[2], (255, 0, 0)],
-                        [self.temp_vertices[0], self.temp_vertices[3], self.temp_vertices[2], (255, 0, 0)],
-                        [self.temp_vertices[4], self.temp_vertices[0], self.temp_vertices[3], (225, 0, 0)],
-                        [self.temp_vertices[4], self.temp_vertices[7], self.temp_vertices[3], (225, 0, 0)],
-                        [self.temp_vertices[1], self.temp_vertices[5], self.temp_vertices[6], (200, 0, 0)],
-                        [self.temp_vertices[1], self.temp_vertices[2], self.temp_vertices[6], (200, 0, 0)],
-                        [self.temp_vertices[5], self.temp_vertices[4], self.temp_vertices[7], (150, 0, 0)],
-                        [self.temp_vertices[5], self.temp_vertices[6], self.temp_vertices[7], (150, 0, 0)],
-                        [self.temp_vertices[0], self.temp_vertices[4], self.temp_vertices[5], (125, 0, 0)],
-                        [self.temp_vertices[0], self.temp_vertices[1], self.temp_vertices[5], (125, 0, 0)],
-                        [self.temp_vertices[3], self.temp_vertices[2], self.temp_vertices[6], (100, 0, 0)],
-                        [self.temp_vertices[3], self.temp_vertices[7], self.temp_vertices[6], (100, 0, 0)],]
+        self.tris =    [[self.temp_vertices[0], self.temp_vertices[1], self.temp_vertices[2], (180, 180, 210)],
+                        [self.temp_vertices[0], self.temp_vertices[2], self.temp_vertices[3], (150, 150, 170)],
+                        [self.temp_vertices[0], self.temp_vertices[1], self.temp_vertices[3], (110, 110, 130)],
+                        [self.temp_vertices[1], self.temp_vertices[2], self.temp_vertices[4], (170, 170, 210)],
+                        [self.temp_vertices[2], self.temp_vertices[3], self.temp_vertices[4], (140, 140, 160)],
+                        [self.temp_vertices[1], self.temp_vertices[5], self.temp_vertices[4], (50, 50, 50)],
+                        [self.temp_vertices[4], self.temp_vertices[5], self.temp_vertices[3], (50, 50, 50)],
+                        [self.temp_vertices[1], self.temp_vertices[5], self.temp_vertices[3], (200, 0, 0)],
+                        [self.temp_vertices[1], self.temp_vertices[6], self.temp_vertices[7], (50, 50, 200)],
+                        [self.temp_vertices[1], self.temp_vertices[9], self.temp_vertices[8], (180, 180, 210)],
+                        [self.temp_vertices[1], self.temp_vertices[8], self.temp_vertices[10], (140, 140, 160)],
+                        [self.temp_vertices[8], self.temp_vertices[10], self.temp_vertices[11], (150, 150, 170)],
+                        [self.temp_vertices[8], self.temp_vertices[9], self.temp_vertices[11], (180, 180, 210)],
+                        [self.temp_vertices[9], self.temp_vertices[10], self.temp_vertices[11], (110, 110, 130)],
+                        [self.temp_vertices[9], self.temp_vertices[10], self.temp_vertices[1], (110, 110, 130)],
+                        [self.temp_vertices[3], self.temp_vertices[12], self.temp_vertices[13], (50, 50, 200)],
+                        [self.temp_vertices[3], self.temp_vertices[15], self.temp_vertices[14], (180, 180, 210)],
+                        [self.temp_vertices[3], self.temp_vertices[14], self.temp_vertices[16], (140, 140, 160)],
+                        [self.temp_vertices[14], self.temp_vertices[16], self.temp_vertices[17], (150, 150, 170)],
+                        [self.temp_vertices[14], self.temp_vertices[15], self.temp_vertices[17], (180, 180, 210)],
+                        [self.temp_vertices[15], self.temp_vertices[16], self.temp_vertices[17], (110, 110, 130)],
+                        [self.temp_vertices[15], self.temp_vertices[16], self.temp_vertices[3], (110, 110, 130)]]
         
         self.filter_tris = []
         
